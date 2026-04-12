862d6741ba6a3f3384f273f093e6600613b77825
R3D <cristophermendezcervantes@gmail.com>
2026-04-12 15:42:06 +0100

    new featured + bootstrap refactor

--- main/main/views.py
+++ main/main/views.py
@@ -4,3 +4,5 @@ def homepage(request):
     return render(request, 'documents/index.html', {})
 def about_us(request):
     return render(request, 'documents/about_us.html', {})
+def traditional_baking(request):
+    return render(request, 'documents/traditional_baking.html', {})
